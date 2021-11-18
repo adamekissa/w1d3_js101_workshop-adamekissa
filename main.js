1fc9e16f1fe8ad5b8ec968997df18369340c38db
adamekissa <adamekissa@gmail.com>
2021-11-18 23:15:26 +0000

    Sunshine

---
+++ main.js
@@ -0,0 +1,48 @@
+// console.log("Hello word");
+
+// if (1 > 3) {
+//   console.log("Well, duh! Of course 1 isn't greater than three...");
+// }
+
+// let numberOfPies = getPieCount();
+
+// if (numberOfPies < 1) {
+//   console.log("Who ate all the pies???");
+// }
+
+//Task 1
+
+// let myFavNumber = 5;
+
+// if (myFavNumber == 5) {
+//   console.log("wow, my favourite number is 5 too!");
+// }
+
+// Task 2
+
+// let askName = prompt("What's your name?");
+// let myName = askName;
+
+// if (myName == "Ben") {
+//   console.log("Great!, It's my name too");
+// } else {
+//   console.log("hum... It's a silly name...");
+// }
+
+// Task 3
+// let weatherTime = "gray";
+
+// if (weatherTime == "Raining") {
+//   console.log("Take an umbrella");
+// } else if (weatherTime == "sunny") {
+//   console.log("Don't forget your suncream!");
+// } else {
+//   console.log("Must be a normal day, grey, non-descript day!");
+// }
+
+function sayHello(name, age) {
+    return "Hello " + name + " You are " + age + " years old";
+  }
+  
+  let gretting = sayHello("Cintia", 29);
+  console.log(gretting);
